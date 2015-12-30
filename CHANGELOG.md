e28d38995ddf0fdbc0ac95a79e161dc55b0dc195
Chris Roberts <code@chrisroberts.org>
2015-12-29 16:30:17 -0800

    Update version for development 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+# Unreleased changes
+
+# v0.1.2
+
 # v0.1.0
 * Initial release
--- lib/sfn-serverspec/version.rb
+++ lib/sfn-serverspec/version.rb
@@ -1,5 +1,5 @@
 # SfnServerspec internal constants
 module SfnServerspec
   # Current version
-  VERSION = Gem::Version.new('0.1.0')
+  VERSION = Gem::Version.new('0.1.1')
 end
